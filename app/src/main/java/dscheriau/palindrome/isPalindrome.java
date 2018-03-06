a2e461fa7e2ed0fee65ad00a3c547ce4e1c31b69
Daniel Scheriau <daniel.scheriau@gmail.com>
2018-03-06 09:56:04 +0100

    initial commit

---
+++ app/src/main/java/dscheriau/palindrome/isPalindrome.java
@@ -0,0 +1,13 @@
+package dscheriau.palindrome;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class isPalindrome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_is_palindrome);
+    }
+}
