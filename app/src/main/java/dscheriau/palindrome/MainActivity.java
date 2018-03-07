501b7630f534c2bfa987871564f37aec6836eafe
Daniel Scheriau <daniel.scheriau@gmail.com>
2018-03-07 21:07:56 +0100

    Init MainActivity with Widgets and OnClickListener Function

--- app/src/main/java/dscheriau/palindrome/MainActivity.java
+++ app/src/main/java/dscheriau/palindrome/MainActivity.java
@@ -2,16 +2,32 @@ package dscheriau.palindrome;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 public class MainActivity extends AppCompatActivity {
 
-
+    Button btnCheck = null;
+    EditText etInput = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_is_palindrome);
+
+        //init Widgets
+        btnCheck = (Button) findViewById(R.id.btnCheck);
+        etInput = (EditText) findViewById(R.id.etInput);
+
+        //Set on click listener which listen to click actions
+        btnCheck.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 
 }
